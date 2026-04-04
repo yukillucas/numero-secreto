a66ebe591f4e05788af4230f1700cc07111931ea
Lucas Ferreira <lucasduarteufrn@gmail.com>
2026-04-04 16:50:29 -0300

    primeira commit

--- Jogo (iniciante)/logica-js-projeto_inicial/app.js
+++ Jogo (iniciante)/logica-js-projeto_inicial/app.js
@@ -1,4 +1,4 @@
-alert('Boas vindas ao jogo do número secreto');
+alert('Boas vindas ao jogo do número secreto rulador');
 let numeroMaximo = 5000;
 let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1); 
 console.log(numeroSecreto);
